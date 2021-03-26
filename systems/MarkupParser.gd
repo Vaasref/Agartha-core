9cb0aafbef4d3f771c390ecd7a1f85e351680a57
Vaasref <me@vaasref.io>
2021-03-26 16:17:55 +0100

    Improve markup parser to support line feed.

--- systems/MarkupParser.gd
+++ systems/MarkupParser.gd
@@ -9,13 +9,28 @@ func parse_text(text:String, store=null):
 	
 	output = hidden_escape(output)
 	output = output.format(store.properties, "{_}")
+	output = replace_line_feed(output)
 	
 	return output
+
+
+func replace_line_feed(text:String):
+	var output = text
+	var re = RegEx.new()
+	re.compile("(?<!\\\\)\\\\n")
+	var results = re.search_all(text)
+	var offset = 0
+	for m in results:
+		output = "%s\n%s" % [output.substr(0, offset + m.get_start(0)), output.substr(offset + m.get_end(0), -1)]
+		offset = output.length() - text.length()
+	
+	return output
+
 
 func hidden_escape(text:String):
 	var output = text
 	var re = RegEx.new()
-	re.compile("(?<!\\\\)\\\\([^\\\\])")
+	re.compile("(?<!\\\\)\\\\([^n\\\\])")
 	var results = re.search_all(text)
 	var offset = 0
 	for m in results:
