8a19ea4b2e63c43b3ac3d8baeeb8626563885ca0
Vaasref <me@vaasref.io>
2021-04-15 14:57:05 +0200

    Rewrote Show_Hide, it actually works now.

--- systems/Show_Hide.gd
+++ systems/Show_Hide.gd
@@ -30,81 +30,101 @@ func _get_character_parameters(first_tag_element:String):
 
 ## Shown tag management
 
-var shown = {} setget _set_shown, _get_shown
 
-func _set_shown(value):
-	Agartha.store.set("_show", value)
+func _store(state):
+	var sh_states:Dictionary = {}
+	for n in get_tree().get_nodes_in_group("tagged"):
+		var entry:Dictionary = {}
+		var visible = null
+		if n is CanvasItem:
+			entry['visible'] = n.visible
+		if n.has_meta("_show_tag"):
+			entry['_show_tag'] = n.get_meta("_show_tag")
+			entry['_show_parameters'] = n.get_meta("_show_parameters")
+		elif n.has_meta("_hide_tag"):
+			entry['_hide_tag'] = n.get_meta("_hide_tag")
+			entry['_hide_parameters'] = n.get_meta("_hide_parameters")
+		sh_states[_get_truncated_path(n)] = entry
+		
+	state.set("_tagged_sh_states", sh_states)
 
-func _get_shown():
-	if Agartha.store.has("_show"):
-		Agartha.store.get("_show")
-	else:
-		return {}
-	
+func _get_truncated_path(node:Node):
+	return "." + str(node.get_path()).trim_prefix(str(Agartha.stage.get_path()))
+
+
+func _restore(state):
+	var sh_states:Dictionary = state.get("_tagged_sh_states")
+	for path in sh_states.keys():
+		var n = Agartha.stage.get_node(path)
+		if n:
+			if '_show_tag' in sh_states[path]:
+				if 'visible' in sh_states[path] and sh_states[path]['visible']:
+					show_node(n, sh_states[path]['_show_tag'], sh_states[path]['_show_parameters'])
+				else:
+					n.set_meta("_show_tag", sh_states[path]['_show_tag'])
+					n.set_meta("_show_parameters", sh_states[path]['_show_parameters'])
+			elif '_hide_tag' in sh_states[path]:
+				if 'visible' in sh_states[path] and not sh_states[path]['visible']:
+					hide_node(n, sh_states[path]['_hide_tag'], sh_states[path]['_hide_parameters'])
+				else:
+					n.set_meta("_hide_tag", sh_states[path]['_hide_tag'])
+					n.set_meta("_hide_parameters", sh_states[path]['_hide_parameters'])
+			elif n is CanvasItem and 'visible' in sh_states[path]:
+				n.visible = sh_states[path]['visible']
+
 
 func show(tag:String, parameters:Dictionary):
 	tag = "# " + tag.trim_prefix("# ")
 	var radical = tag.split(" ", false, 2)[1]
-	var dict_to_show = Agartha.Tag.get_node_dict(tag)
-	var nodes_to_show = []
-	var new_shown = {}
-	for t in dict_to_show.keys():
-		nodes_to_show += dict_to_show[t]
-		new_shown[t] = true
-	shown[radical] = new_shown
+	var to_show = Agartha.Tag.get_array(tag) # Might be better as a dictionary for a big number of entries.
 	
-	var family = Agartha.Tag.get_array(radical)
-	for n in family:
-		if n :
-			if n in nodes_to_show:
-				if not n.get("visible"):
-					if n.has_method("_show"):
-						n.call("_show", tag, parameters)
-					elif n.has_method("show"):
-						n.call("show")
-			else:
-				if n.has_method("_hide"):
-					n.call("_hide", tag, parameters)
-				elif n.has_method("hide"):
-					n.call("hide")
+	for n in Agartha.Tag.get_array(radical):
+		if n in to_show:
+			show_node(n, tag, parameters)
+		else:
+			hide_node(n, tag, parameters)
 
-	
+
 func hide(tag:String, parameters:Dictionary):
 	tag = "# " + tag.trim_prefix("# ")
-	var radical = tag.split(" ", false, 2)[1]
-	var dict_to_hide = Agartha.Tag.get_node_dict(tag)
-	var nodes_to_hide = []
-	var new_shown
-	if radical in shown:
-		new_shown = shown[radical].duplicate()
-	else:
-		new_shown = {}
-	for t in dict_to_hide.keys():
-		new_shown.erase(t)
-		nodes_to_hide += dict_to_hide[t]
-	if new_shown:
-		shown[radical] = new_shown
-	else:
-		shown.erase(radical)
-	
-	var family = Agartha.Tag.get(radical)
-	if family is Array:
-		for n in family:
-			if n:
-				if n in nodes_to_hide:
-					if n.has_method("_hide"):
-						n.call("_hide", tag, parameters)
-					elif n.has_method("hide"):
-						n.call("hide")
-	else:
-		if family in nodes_to_hide:
-			if family.has_method("_hide"):
-				family.call("_hide", tag, parameters)
-			elif family.has_method("hide"):
-				family.call("hide")
-	
-	
-	
-	
-	
-	
+	var to_hide = Agartha.Tag.get_array(tag)
+	for n in to_hide:
+		hide_node(n, tag, parameters)
+
+
+
+func show_node(node:Node, tag:String, parameters:Dictionary):
+	if node:
+		if node.has_method("show") or node.has_method("_show"):
+			node.set_meta("_show_tag", tag)
+			node.set_meta("_show_parameters", parameters)
+			if node.has_meta("_hide_tag"):
+				node.set_meta("_hide_tag", null)
+				node.set_meta("_hide_parameters", null)
+			
+		if node.has_method("_show"):
+			node.call("_show", tag, parameters)
+		elif node.has_method("show"):
+			node.call("show")
+
+
+func hide_node(node:Node, tag:String, parameters:Dictionary):
+	if node:
+		if node.has_method("hide") or node.has_method("_hide"):
+			node.set_meta("_hide_tag", tag)
+			node.set_meta("_hide_parameters", parameters)
+			if node.has_meta("_show_tag"):
+				node.set_meta("_show_tag", null)
+				node.set_meta("_show_parameters", null)
+			
+		if node.has_method("_hide"):
+			node.call("_hide", tag, parameters)
+		elif node.has_method("hide"):
+			node.call("hide")
+
+
+
+
+
+
+
