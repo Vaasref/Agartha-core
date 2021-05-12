c679f02c56db2a9c5d5c4d593990d2ae7e609864
Vaasref <me@vaasref.io>
2021-05-12 04:21:12 +0200

    Adding complex names variable replace to markup parser.

--- systems/MarkupParser.gd
+++ systems/MarkupParser.gd
@@ -1,6 +1,6 @@
 extends Node
 
-
+onready var expr = Expression.new()
 
 func parse_text(text:String, store=null):
 	var output = text
@@ -8,10 +8,36 @@ func parse_text(text:String, store=null):
 		store = Agartha.store
 	
 	output = hidden_escape(output)
-	output = output.format(store.properties, "{_}")
+	output = replace_store_variables(output, store)
 	output = replace_line_feed(output)
 	
 	return output
+
+
+func replace_store_variables(text:String, store):
+	if store:
+		var output = text
+		var re = RegEx.new()
+		re.compile("{(([a-zA-Z]|[a-zA-Z_][a-zA-Z_0-9]+)(\\.\\g<2>)*)}")
+		var results = re.search_all(text)
+		var offset = 0
+		for m in results:
+			var name = m.get_string(1)
+			var value = m.get_string(0)
+			if store.has(name):
+				value = store.get(name)
+			elif "." in name:
+				var error = expr.parse(name)
+				if error == OK:
+					var result = expr.execute([], store, true)
+					if not expr.has_execute_failed():
+						value = result
+
+			output = "%s%s%s" % [output.substr(0, offset + m.get_start(0)), value, output.substr(offset + m.get_end(0), -1)]
+			offset = output.length() - text.length()
+		return output
+	else:
+		return text
 
 
 func replace_line_feed(text:String):
